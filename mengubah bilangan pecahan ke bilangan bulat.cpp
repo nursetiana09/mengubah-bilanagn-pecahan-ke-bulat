29bceea44ceb1c44ca3307b38817b4c97be177e7
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 12:16:26 +0700

    Add files via upload

---
+++ mengubah bilangan pecahan ke bilangan bulat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+ int a; //mendefinisikan variabel a sebagai int (integer/ bilangan bulat)
+
+ cout<< "Masukam angka pecahan (pakai .) = " ;
+ cin >> a;
+
+ cout << "bilangan bulatnya adalah = " << a << endl;
+
+
+ return 0;
+
+}
+
